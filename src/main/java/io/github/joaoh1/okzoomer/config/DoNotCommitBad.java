626a9a95f8283c3ff5ce645e1616c2699b65d3dc
Past Ennui <dont_worry.you_will_escape_this_darkness@example.com>
2020-05-09 19:51:20 -0300

    Improve the anti-bad

--- src/main/java/io/github/joaoh1/okzoomer/config/DoNotCommitBad.java
+++ src/main/java/io/github/joaoh1/okzoomer/config/DoNotCommitBad.java
@@ -2,57 +2,70 @@ package io.github.joaoh1.okzoomer.config;
 
 //TODO - Get rid of this workaround after Fiber is fixed
 public class DoNotCommitBad {
+    private static String pastCinematicZoomValue = "off";
+    private static boolean pastReduceSensitivityValue = true;
+    private static boolean pastSmoothTransitionValue = true;
+    private static boolean pastZoomToggleValue = false;
+    private static boolean pastZoomScrollingValue = false;
+    private static double pastZoomDivisorValue = 4.0D;
+    
     public static final String getCinematicZoom() {
         String configValue = OkZoomerConfig.cinematicCamera.getValue();
         if (configValue != null) {
+            pastCinematicZoomValue = configValue;
             return configValue;
         } else {
-            return "off";
+            return pastCinematicZoomValue;
         }
     }
 
     public static final boolean getReduceSensitivity() {
         Boolean configValue = OkZoomerConfig.reduceSensitivity.getValue();
         if (configValue != null) {
+            pastReduceSensitivityValue = configValue;
             return configValue;
         } else {
-            return false;
+            return pastReduceSensitivityValue;
         }
     }
 
     public static final boolean getSmoothTransition() {
         Boolean configValue = OkZoomerConfig.smoothTransition.getValue();
         if (configValue != null) {
+            pastSmoothTransitionValue = configValue;
             return configValue;
         } else {
-            return false;
+            return pastSmoothTransitionValue;
         }
     }
 
     public static final boolean getZoomToggle() {
         Boolean configValue = OkZoomerConfig.zoomToggle.getValue();
         if (configValue != null) {
+            pastZoomToggleValue = configValue;
             return configValue;
         } else {
-            return false;
+            return pastZoomToggleValue;
         }
     }
 
     public static final double getZoomDivisor() {
         Double configValue = OkZoomerConfig.zoomDivisor.getValue();
         if (configValue != null) {
+            pastZoomDivisorValue = configValue;
             return configValue;
         } else {
-            return 4.0D;
+            return pastZoomDivisorValue;
         }
     }
 
     public static final boolean getZoomScrolling() {
         Boolean configValue = OkZoomerConfig.zoomScrolling.getValue();
         if (configValue != null) {
+            pastZoomScrollingValue = configValue;
             return configValue;
         } else {
-            return false;
+            return pastZoomScrollingValue;
         }
     }
 }
