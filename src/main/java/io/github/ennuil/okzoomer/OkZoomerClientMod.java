860034ef56c9d592f1a39867acf222eab78b8368
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2022-03-09 19:02:29 -0300

    Fix key binds not appearing; beta.5 scream

--- src/main/java/io/github/ennuil/okzoomer/OkZoomerClientMod.java
+++ src/main/java/io/github/ennuil/okzoomer/OkZoomerClientMod.java
@@ -3,13 +3,23 @@ package io.github.ennuil.okzoomer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 import io.github.ennuil.okzoomer.commands.OkZoomerCommands;
+import io.github.ennuil.okzoomer.key_binds.ZoomKeyBinds;
 import io.github.ennuil.okzoomer.packets.ZoomPackets;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.ModContainer;
 
 // This class is responsible for registering the commands and packets
 public class OkZoomerClientMod implements ClientModInitializer {
     @Override
     public void onInitializeClient(ModContainer container) {
+        // Register all the key binds
+        KeyBindingHelper.registerKeyBinding(ZoomKeyBinds.ZOOM_KEY);
+        if (ZoomKeyBinds.areExtraKeyBindsEnabled()) {
+            KeyBindingHelper.registerKeyBinding(ZoomKeyBinds.DECREASE_ZOOM_KEY);
+            KeyBindingHelper.registerKeyBinding(ZoomKeyBinds.INCREASE_ZOOM_KEY);
+            KeyBindingHelper.registerKeyBinding(ZoomKeyBinds.RESET_ZOOM_KEY);
+        }
+        
         // Register the commands
         OkZoomerCommands.registerEvent();
 
--- src/main/java/io/github/ennuil/okzoomer/key_binds/ZoomKeyBinds.java
+++ src/main/java/io/github/ennuil/okzoomer/key_binds/ZoomKeyBinds.java
@@ -5,38 +5,40 @@ import com.mojang.blaze3d.platform.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 import io.github.ennuil.okzoomer.config.OkZoomerConfigManager;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBind;
 
 // Manages the zoom keybinds themselves
 public class ZoomKeyBinds {
+    // The "Zoom" category
+    public static final String ZOOM_CATEGORY = "key.okzoomer.category";
+
     // The zoom key bind, which will be registered
-    public static final KeyBind ZOOM_KEY = KeyBindingHelper.registerKeyBinding(
-        new KeyBind("key.okzoomer.zoom", GLFW.GLFW_KEY_C, "key.okzoomer.category"));
+    public static final KeyBind ZOOM_KEY = new KeyBind("key.okzoomer.zoom", GLFW.GLFW_KEY_C, ZOOM_CATEGORY);
     
     // The "Decrease Zoom" key bind
-    public static final KeyBind DECREASE_ZOOM_KEY = getExtraKeybind("key.okzoomer.decrease_zoom");
+    public static final KeyBind DECREASE_ZOOM_KEY = getExtraKeyBind("key.okzoomer.decrease_zoom");
 
     // The "Increase Zoom" key bind
-    public static final KeyBind INCREASE_ZOOM_KEY = getExtraKeybind("key.okzoomer.increase_zoom");
+    public static final KeyBind INCREASE_ZOOM_KEY = getExtraKeyBind("key.okzoomer.increase_zoom");
 
     // The "Reset Zoom" key bind
-    public static final KeyBind RESET_ZOOM_KEY = getExtraKeybind("key.okzoomer.reset_zoom");
+    public static final KeyBind RESET_ZOOM_KEY = getExtraKeyBind("key.okzoomer.reset_zoom");
 
     // The method used to check if the zoom manipulation key binds should be disabled, can be used by other mods.
     public static final boolean areExtraKeyBindsEnabled() {
         if (!OkZoomerConfigManager.isConfigLoaded.isPresent()) {
             OkZoomerConfigManager.loadModConfig();
         }
+        
         return OkZoomerConfigManager.configInstance.features().getExtraKeyBinds();
     }
     
     // The method used to get the extra keybinds, if disabled, return null.
-    public static final KeyBind getExtraKeybind(String translationKey) {
+    public static final KeyBind getExtraKeyBind(String translationKey) {
         if (areExtraKeyBindsEnabled()) {
-            return KeyBindingHelper.registerKeyBinding(
-                new KeyBind(translationKey, InputUtil.UNKNOWN_KEY.getKeyCode(), "key.okzoomer.category"));
+            return new KeyBind(translationKey, InputUtil.UNKNOWN_KEY.getKeyCode(), ZOOM_CATEGORY);
         }
+
         return null;
     }
 }
