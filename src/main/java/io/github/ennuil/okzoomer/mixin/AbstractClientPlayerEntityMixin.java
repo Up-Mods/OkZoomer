deda806ee521ea79937f844ddae4ca46e27a75e8
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2022-04-08 23:25:16 -0300

    Use MixinExtras, get haunted by ghost again If everything goes right, then we'll finally have a release tomorrow! Stay tuned!

--- src/main/java/io/github/ennuil/okzoomer/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/io/github/ennuil/okzoomer/mixin/AbstractClientPlayerEntityMixin.java
@@ -1,20 +1,20 @@
 package io.github.ennuil.okzoomer.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import io.github.ennuil.okzoomer.packets.ZoomPackets;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 
-// TODO - I hate Redirects; How am I going to get rid of it?
 @Mixin(AbstractClientPlayerEntity.class)
 public abstract class AbstractClientPlayerEntityMixin {
-    @Redirect(
+    @ModifyExpressionValue(
         at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;isUsingSpyglass()Z"),
         method = "getSpeed"
     )
-    private boolean replaceSpyglassMouseMovement(AbstractClientPlayerEntity player) {
+    private boolean replaceSpyglassMouseMovement(boolean isUsingSpyglass) {
         if (switch (ZoomPackets.getSpyglassDependency()) {
             case REPLACE_ZOOM -> true;
             case BOTH -> true;
@@ -22,7 +22,7 @@ public abstract class AbstractClientPlayerEntityMixin {
         }) {
             return false;
         } else {
-            return player.isUsingSpyglass();
+            return isUsingSpyglass;
         }
     }
 }
--- src/main/java/io/github/ennuil/okzoomer/mixin/InGameHudMixin.java
+++ src/main/java/io/github/ennuil/okzoomer/mixin/InGameHudMixin.java
@@ -1,20 +1,20 @@
 package io.github.ennuil.okzoomer.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import io.github.ennuil.okzoomer.packets.ZoomPackets;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.network.ClientPlayerEntity;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
-    @Redirect(
+    @ModifyExpressionValue(
         at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isUsingSpyglass()Z"),
         method = "render"
     )
-    private boolean replaceSpyglassMouseMovement(ClientPlayerEntity player) {
+    private boolean replaceSpyglassMouseMovement(boolean isUsingSpyglass) {
         if (switch (ZoomPackets.getSpyglassDependency()) {
             case REPLACE_ZOOM -> true;
             case BOTH -> true;
@@ -22,7 +22,7 @@ public abstract class InGameHudMixin {
         }) {
             return false;
         } else {
-            return player.isUsingSpyglass();
+            return isUsingSpyglass;
         }
     }
 }
