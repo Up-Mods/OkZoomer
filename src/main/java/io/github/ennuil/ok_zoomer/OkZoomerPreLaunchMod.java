4624bedcb7f20b6a479984657876713ddd925b4b
Up <upcraftedlp@gmail.com>
2024-03-30 22:16:25 +0100

    no need to bootstrap mixinextras anymore

--- src/main/java/io/github/ennuil/ok_zoomer/OkZoomerPreLaunchMod.java
+++ src/main/java/io/github/ennuil/ok_zoomer/OkZoomerPreLaunchMod.java
@@ -11,7 +11,6 @@ import io.github.ennuil.ok_zoomer.config.metadata.WidgetSize;
 public class OkZoomerPreLaunchMod implements PreLaunchEntrypoint {
 	@Override
 	public void onPreLaunch(ModContainer mod) {
-		MixinExtrasBootstrap.init();
 		ConfigFieldAnnotationProcessor.register(WidgetSize.class, new WidgetSize.Processor());
 	}
 }
