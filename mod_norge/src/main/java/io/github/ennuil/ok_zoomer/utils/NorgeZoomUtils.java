e7f6f9876d4001cae385f55bd441690f93bfa8bf
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2025-04-08 15:57:33 -0300

    Switch to `contains(TagKey<Item>)` Predicate testing is silly here, for now

--- mod_norge/src/main/java/io/github/ennuil/ok_zoomer/utils/NorgeZoomUtils.java
+++ mod_norge/src/main/java/io/github/ennuil/ok_zoomer/utils/NorgeZoomUtils.java
@@ -1,16 +1,13 @@
 package io.github.ennuil.ok_zoomer.utils;
 
-import net.minecraft.world.item.ItemStack;
 import net.neoforged.fml.ModList;
-
-import java.util.function.Predicate;
 
 public class NorgeZoomUtils {
 	// TODO - Bad! We need client tags for this!
-	public static final Predicate<ItemStack> IS_VALID_SPYGLASS = stack -> stack.is(ZoomUtils.ZOOM_DEPENDENCIES_TAG);
+	//public static final Predicate<ItemStack> IS_VALID_SPYGLASS = stack -> stack.is(ZoomUtils.ZOOM_DEPENDENCIES_TAG);
 
 	public static void addInitialPredicates() {
-		ZoomUtils.addSpyglassProvider(player -> player.getInventory().contains(IS_VALID_SPYGLASS));
+		ZoomUtils.addSpyglassProvider(player -> player.getInventory().contains(ZoomUtils.ZOOM_DEPENDENCIES_TAG));
 	}
 
 	public static void defineSafeSmartOcclusion() {
