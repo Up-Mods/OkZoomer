27c4b2875c1bc999e4b14a07e0ef1246d2da136a
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2025-06-06 15:10:02 -0300

    Add condolence owo So sorry for *everything*

--- mod_common/src/main/java/io/github/ennuil/ok_zoomer/utils/OwoUtils.java
+++ mod_common/src/main/java/io/github/ennuil/ok_zoomer/utils/OwoUtils.java
@@ -14,7 +14,8 @@ public class OwoUtils {
 		".w.", "+w+", ")w(", "]w[", "}w{", "_w_", "=w=", "!w!",
 		"YwY", "vwv", "VwV", "<w>", "çwç", "ÇwÇ", ">w>", "<w<",
 		"—w—", "→w→", "→w←", "←w←", "KwK", "GwG", "gwg", "qwq",
-		"AwA", "awa", "\\w/", "…w…", "ªwª", "—w—", "¯w¯"
+		"AwA", "awa", "\\w/", "…w…", "ªwª", "—w—", "¯w¯", "XwX",
+		"xwx"
 	};
 
 	public static void printOwo() {
