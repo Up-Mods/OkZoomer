4284922807620e09877c10f209924b5a38f71fd3
Ennui Langeweile <ennui@langeweile.page>
2026-09-14 22:11:38 -0300

    Port to NeoForge 26.3 Yes it's broken, but I don't care. Ship it now!!!!!!!!!!!

--- mod/build.gradle.kts
+++ mod/build.gradle.kts
@@ -32,11 +32,9 @@ dependencies {
 	implementation(project(":mod_fabric")) {
 		isTransitive = false
 	}
-	/*
 	implementation(project(":mod_norge")) {
 		isTransitive = false
 	}
-	 */
 }
 
 // TODO - Somehow make a source JAR from all of this
